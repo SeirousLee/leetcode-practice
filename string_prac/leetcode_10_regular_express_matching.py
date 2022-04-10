2f65a6eaa95b2a0ae1ab011701c89da7ff8114d4
SeirousLee <lx10702@sina.com>
2022-04-10 14:25:11 +0800

    正则表达式匹配

--- string_prac/leetcode_10_regular_express_matching.py
+++ string_prac/leetcode_10_regular_express_matching.py
@@ -27,6 +27,37 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
 
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        pass
+        m, n = len(s), len(p)
+
+        def matches(i: int, j: int) -> bool:
+            if i == 0:
+                return False
+            # . 表示任意字符都匹配
+            if p[j - 1] == '.':
+                return True
+            return s[i - 1] == p[j - 1]
+
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+        for i in range(m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    # dp[i][j]表示s的前i个字符是否与p的前j个字符是否匹配
+                    # 当*表示0个匹配是dp[i][j]是否匹配与dp[i][j - 2]相关
+                    dp[i][j] = dp[i][j - 2]
+                    # 匹配到1个或多个是dp[i][j]是否匹配与dp[i-1][j]相关，因为* 是0个或多个匹配，所以使用|=
+                    if matches(i, j - 1):
+                        dp[i][j] |= dp[i - 1][j]
+                else:
+                    # 当不含*时，dp[i][j]的匹配性只与dp[i - 1][j - 1]相关
+                    if matches(i, j):
+                        dp[i][j] = dp[i - 1][j - 1]
+        return dp[m][n]
+
+if __name__ == '__main__':
+    s=Solution()
+    res = s.isMatch("aaa", "ab*a*c*a")
+    print(res)
