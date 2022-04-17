ed274f76973f2f07d58ffdabed6202608e548b80
SeirousLee <lx10702@sina.com>
2022-04-17 16:25:29 +0800

    括号生成

--- leetcode22_Generate_Parentheses.py
+++ leetcode22_Generate_Parentheses.py
@@ -20,17 +20,19 @@ from typing import List
 
 
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+    def generateParenthesis_backtrack(self, n: int) -> List[str]:
         res = []
 
         def backtrack(s, left, right):
             if len(s) == 2 * n:
                 res.append(''.join(s))
                 return
+            # 深度优先遍历(
             if left < n:
                 s.append('(')
                 backtrack(s, left + 1, right)
                 s.pop()
+            # 为保证）能够匹配（ 需要保证right < left
             if right < left:
                 s.append(')')
                 backtrack(s, left, right + 1)
@@ -38,6 +40,16 @@ class Solution:
 
         backtrack([], 0, 0)
         return res
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 1:
+            return ['()']
+        res = set()
+        # 在知道前一个数量的括号表达式后，向表达式中拼接"()"即可，表达式仍为正常括号表达式
+        for i in self.generateParenthesis(n - 1):
+            for j in range(len(i)):
+                res.add(i[0:j] + '()' + i[j:])
+        return list(res)
 
 
 if __name__ == '__main__':
